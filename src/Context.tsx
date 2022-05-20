be79dc5f1c98ca40d24c37cf3bb9c4b9160fcd9a
straxissosad <skrytyya@gmail.com>
2022-05-20 07:33:06 +0500

    context

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,18 @@
-import { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState, useContext }from 'react';
+
 
 type Theme = 'light' | 'dark';
+const contextTheme = React.createContext<Theme>('light');
 
 export function ThemeProvider(props: {
     theme: Theme;
     children: ReactElement;
-}) {}
+}) {
+    return (
+        <contextTheme.Provider value={props.theme}>{props.children}</contextTheme.Provider>
+    );
+}
 
-export function useTheme(): Theme {}
+export function useTheme(): Theme {
+    return useContext(contextTheme);
+}
